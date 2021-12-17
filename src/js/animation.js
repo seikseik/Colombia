814bca5bbcdd69d0a29fc74627c450e0ffd4471e
Matteo Sacchi <matteoseik@hotmail.it>
2021-12-17 15:35:21 +0100

    fix scroll

--- src/js/animation.js
+++ src/js/animation.js
@@ -61,3 +61,47 @@ gsap.registerPlugin(ScrollToPlugin, ScrollTrigger, SplitText);
       once: true,
     });
   });
+
+
+
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    const myTimeout = setTimeout(scroll, 1000);
+      function scroll(){
+
+        const sections = gsap.utils.toArray(".panel");
+        let maxWidth = 0;
+
+        const getMaxWidth = () => {
+          maxWidth = 0;
+          sections.forEach((section) => {
+            maxWidth += section.offsetWidth;
+          });
+        };
+        getMaxWidth();
+        ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
+
+        gsap.to(sections, {
+          y: -100,
+          x: () => `-${maxWidth - window.innerWidth}`,
+          ease: "none",
+          scrollTrigger: {
+            trigger: ".slider-section",
+            pin: true,
+            scrub: true,
+            end: () => `+=${maxWidth}`,
+            invalidateOnRefresh: true
+          }
+        });
+
+        sections.forEach((sct, i) => {
+          ScrollTrigger.create({
+            trigger: sct,
+            start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth/2) * (maxWidth / (maxWidth - window.innerWidth)),
+            end: () => '+=' + sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth)),
+            toggleClass: {targets: sct, className: "active"}
+          });
+        });
+      }
+
+  });
